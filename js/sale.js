f6e4ce7d348ec903bb8e005767989cb95bd0ff28
Lars Grote <lgr@softwerke.com>
2024-06-04 11:57:52 +0200

    add purchase tracking and rtg

---
+++ js/sale.js
@@ -0,0 +1,40 @@
+
+
+
+
+const createClickHandler = () => {
+    return (e) => {
+   
+        console.log('purchase-event');
+        gtag("event", "purchase", {
+            transaction_id: "T_12345",
+            // Sum of (price * quantity) for all items.
+            value: 19.98,
+            tax: 0.0,
+            shipping: 5.99,
+            currency: "EUR",
+    
+            items: [
+             {
+              item_id: "SKU-12345",
+              item_name: "Product One",
+              affiliation: "Google Merchandise Store",
+              index: 0,
+              item_brand: "Google",
+              item_category: "Apparel",
+              item_list_id: "search_result",
+              item_list_name: "Search Result Page",
+              item_variant: "green",
+              price: 9.99,
+              quantity: 2
+            }]
+        });
+}
+}
+
+
+document.querySelectorAll('#contact').forEach(
+    (c) => {
+        c.addEventListener('mousedown', createClickHandler());
+    });
+
